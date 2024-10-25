36595d25b20d6919775c225ef51f798bd8241685
Emerson Lima <emersonlimago123@gmail.com>
2024-10-25 16:21:11 -0300

    finished desktop design

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import plusSign from "./images/icon-plus.svg";
 import minusSign from "./images/icon-minus.svg";
 import replyIcon from "./images/icon-reply.svg";
 import deleteIcon from "./images/icon-delete.svg";
+import editIcon from "./images/icon-edit.svg";
 
 function App() {
   return (
@@ -16,111 +17,171 @@ function App() {
       <main className="commments">
         <div className="comments__commentbox">
           <div className="comments__comment">
-            <div className="comment__authorinfo">
-              <img src={profileCommentOne} alt="profile picture" />
-              <p className="comment__author">amyrobson</p>
-              <p className="comment__timestamp">1 month ago</p>
+            <div>
+              <div className="comment__topsection">
+                <div className="comment__authorinfo">
+                  <img src={profileCommentOne} alt="profile picture" />
+                  <p className="comment__author">amyrobson</p>
+                  <p className="comment__timestamp">1 month ago</p>
+                </div>
+                <button className="comment__replybox commment__replybox--desktop">
+                  <img src={replyIcon} alt="reply icon" />
+                  <span>Reply</span>
+                </button>
+              </div>
+              <p className="comment__content">
+                Impressive! Though it seems the drag feature could be improved.
+                But overall it looks incredible. You've nailed the design and
+                the responsiveness at various breakpoints works really well.
+              </p>
             </div>
-            <p className="comment__content">
-              Impressive! Though it seems the drag feature could be improved.
-              But overall it looks incredible. You've nailed the design and the
-              responsiveness at various breakpoints works really well.
-            </p>
+
             <div className="comment__actions">
               <div className="comment__upvotebox">
-                <img src={plusSign} alt="plus sign" />
+                <button>
+                  <img src={plusSign} alt="plus sign" />
+                </button>
+
                 <span>12</span>
-                <img src={minusSign} alt="minus sign" />
+                <button>
+                  <img src={minusSign} alt="minus sign" />
+                </button>
               </div>
-              <div className="comment__replybox">
+              <button className="comment__replybox">
                 <img src={replyIcon} alt="reply icon" />
                 <span>Reply</span>
-              </div>
+              </button>
             </div>
           </div>
           <ul className="comment__replies"></ul>
         </div>
         <div className="comments__commentbox">
           <div className="comments__comment">
-            <div className="comment__authorinfo">
-              <img src={profileCommentTwo} alt="profile picture" />
-              <p className="comment__author">maxblagun</p>
-              <p className="comment__timestamp">2 weeks ago</p>
+            <div>
+              <div className="comment__topsection">
+                <div className="comment__authorinfo">
+                  <img src={profileCommentTwo} alt="profile picture" />
+                  <p className="comment__author">maxblagun</p>
+                  <p className="comment__timestamp">2 weeks ago</p>
+                </div>
+                <button className="comment__replybox  commment__replybox--desktop">
+                  <img src={replyIcon} alt="reply icon" />
+                  <span>Reply</span>
+                </button>
+              </div>
+              <p className="comment__content">
+                Woah, your project looks awesome! How long have you been coding
+                for? I'm still new, but think I want to dive into React as well
+                soon. Perhaps you can give me an insight on where I can learn
+                React? Thanks!
+              </p>
             </div>
-            <p className="comment__content">
-              Woah, your project looks awesome! How long have you been coding
-              for? I'm still new, but think I want to dive into React as well
-              soon. Perhaps you can give me an insight on where I can learn
-              React? Thanks!
-            </p>
+
             <div className="comment__actions">
               <div className="comment__upvotebox">
-                <img src={plusSign} alt="plus sign" />
+                <button>
+                  <img src={plusSign} alt="plus sign" />
+                </button>
+
                 <span>5</span>
-                <img src={minusSign} alt="minus sign" />
+                <button>
+                  <img src={minusSign} alt="minus sign" />
+                </button>
               </div>
-              <div className="comment__replybox">
+              <button className="comment__replybox">
                 <img src={replyIcon} alt="reply icon" />
                 <span>Reply</span>
-              </div>
+              </button>
             </div>
           </div>
           <ul className="comment__replies">
             <li>
               <div className="comments__comment">
-                <div className="comment__authorinfo">
-                  <img src={profileCommentThree} alt="profile picture" />
-                  <p className="comment__author">ramsesmiron</p>
-                  <p className="comment__timestamp">1 week ago</p>
+                <div>
+                  <div className="comment__topsection">
+                    <div className="comment__authorinfo">
+                      <img src={profileCommentThree} alt="profile picture" />
+                      <p className="comment__author">ramsesmiron</p>
+                      <p className="comment__timestamp">1 week ago</p>
+                    </div>
+                    <button className="comment__replybox  commment__replybox--desktop">
+                      <img src={replyIcon} alt="reply icon" />
+                      <span>Reply</span>
+                    </button>
+                  </div>
+                  <p className="comment__content">
+                    <span className="mention">@maxblagun</span> If you're still
+                    new, I'd recommend focusing on the fundamentals of HTML,
+                    CSS, and JS before considering React. It's very tempting to
+                    jump ahead but lay a solid foundation first.
+                  </p>
                 </div>
-                <p className="comment__content">
-                  If you're still new, I'd recommend focusing on the
-                  fundamentals of HTML, CSS, and JS before considering React.
-                  It's very tempting to jump ahead but lay a solid foundation
-                  first.
-                </p>
+
                 <div className="comment__actions">
                   <div className="comment__upvotebox">
-                    <img src={plusSign} alt="plus sign" />
+                    <button>
+                      <img src={plusSign} alt="plus sign" />
+                    </button>
                     <span>4</span>
-                    <img src={minusSign} alt="minus sign" />
+                    <button>
+                      <img src={minusSign} alt="minus sign" />
+                    </button>
                   </div>
-                  <div className="comment__replybox">
+                  <button className="comment__replybox">
                     <img src={replyIcon} alt="reply icon" />
                     <span>Reply</span>
-                  </div>
+                  </button>
                 </div>
               </div>
             </li>
             <li>
               <div className="comments__comment">
-                <div className="comment__authorinfo">
-                  <img src={profileCommentFour} alt="profile picture" />
-                  <p className="comment__author">juliusomo</p>
-                  <span className="user__mark">you</span>
-                  <p className="comment__timestamp">2 days ago</p>
+                <div>
+                  <div className="comment__topsection">
+                    <div className="comment__authorinfo">
+                      <img src={profileCommentFour} alt="profile picture" />
+                      <p className="comment__author">juliusomo</p>
+                      <span className="user__mark">you</span>
+                      <p className="comment__timestamp">2 days ago</p>
+                    </div>
+                    <div className="comment__crudactions comment__crudactions--desktop">
+                      <button className="comment__deletebox">
+                        <img src={deleteIcon} alt="reply icon" />
+                        <span>Delete</span>
+                      </button>
+                      <button className="comment__replybox  commment__replybox--desktop">
+                        <img src={editIcon} alt="edit icon" />
+                        <span>Reply</span>
+                      </button>
+                    </div>
+                  </div>
+                  <p className="comment__content">
+                    <span className="mention">@ramsesmiron</span>I couldn't
+                    agree more with this. Everything moves so fast and it always
+                    seems like everyone knows the newest library/framework. But
+                    the fundamentals are what stay constant.
+                  </p>
                 </div>
-                <p className="comment__content">
-                  I couldn't agree more with this. Everything moves so fast and
-                  it always seems like everyone knows the newest
-                  library/framework. But the fundamentals are what stay
-                  constant.
-                </p>
+
                 <div className="comment__actions">
                   <div className="comment__upvotebox">
-                    <img src={plusSign} alt="plus sign" />
+                    <button>
+                      <img src={plusSign} alt="plus sign" />
+                    </button>
                     <span>2</span>
-                    <img src={minusSign} alt="minus sign" />
+                    <button>
+                      <img src={minusSign} alt="minus sign" />
+                    </button>
                   </div>
                   <div className="comment__crudactions">
-                    <div className="comment__deletebox">
+                    <button className="comment__deletebox">
                       <img src={deleteIcon} alt="reply icon" />
                       <span>Delete</span>
-                    </div>
-                    <div className="comment__replybox">
-                      <img src={replyIcon} alt="reply icon" />
-                      <span>Reply</span>
-                    </div>
+                    </button>
+                    <button className="comment__replybox">
+                      <img src={editIcon} alt="edit icon" />
+                      <span>Edit</span>
+                    </button>
                   </div>
                 </div>
               </div>
